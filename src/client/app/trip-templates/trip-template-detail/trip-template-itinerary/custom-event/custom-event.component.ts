faae1822329f165fb716aadb3b9e41643f45441c
Matias Jerez <jerez.matias@gmail.com>
2018-09-11 18:29:15 -0300

    feat: add new custom event

--- src/client/app/trip-templates/trip-template-detail/trip-template-itinerary/custom-event/custom-event.component.ts
+++ src/client/app/trip-templates/trip-template-detail/trip-template-itinerary/custom-event/custom-event.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {AddEvent} from '../../../../store/trip-template/trip-template.actions';
+import {Event, eventType} from '../../../../shared/models/TripTemplate';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../../store';
 
 @Component({
   selector: 'app-custom-event',
@@ -8,10 +12,13 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CustomEventComponent implements OnInit {
 
+  @Input() isDialog? = false;
+  @Input() dialogRef: any;
   form: FormGroup;
   hours = new Array(24);
   minutes = new Array(60);
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private store: Store<AppState>) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -20,6 +27,33 @@ export class CustomEventComponent implements OnInit {
       hours: '',
       minutes: ''
     });
+  }
+
+  onButtonClick() {
+    const newEvent: Event = this.convertToEvent(eventType.OTHER, 1);
+    this.store.dispatch(new AddEvent(newEvent));
+    this.closeDialog();
+  }
+
+  convertToEvent(event_type: string, order: number): Event {
+    const converted: Event = new Event();
+    const data = this.form.value;
+    console.log('blabalbalbala', data);
+    converted.name = data.name;
+    converted.description = data.description;
+    converted.geo = [];
+    converted.event_type = event_type;
+    converted.ordinal = order || 1;
+    return converted;
+  }
+
+  closeDialog() {
+    if (this.isDialog) {
+      if (this.dialogRef)
+        this.dialogRef.close('CLOSE');
+      return;
+    } else
+      console.log('pepe');
   }
 
 }
