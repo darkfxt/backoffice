7da085e1d7b345e9132ab77e9de064debdc0497a
Matias Jerez <jerez.matias@gmail.com>
2019-01-17 10:13:06 -0300

    WIP

--- src/client/app/routes/route/route-head/route-head.component.ts
+++ src/client/app/routes/route/route-head/route-head.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { TRANSLATE } from '../../../translate-marker';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../store/shared/app.interfaces';
+import {ContentService} from '../../../shared/services/content.service';
+import {HideLoader, ShowLoader} from '../../../store/shared/actions/loader.actions';
 
 @Component({
   selector: 'app-route-head',
@@ -9,7 +13,9 @@ import { TRANSLATE } from '../../../translate-marker';
 })
 export class RouteHeadComponent implements OnInit {
 
-  userLanguage = navigator.language;
+  availableLanguages: Array<string>;
+  defaultLanguage: string;
+  tabController = {};
 
   @Input()
   form: FormGroup;
@@ -39,10 +45,46 @@ export class RouteHeadComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(
+    private contentServiceInstance: ContentService,
+    private store: Store<AppState>
+  ) {
+    this.defaultLanguage = localStorage.getItem('uiLanguage') || navigator.language.split('-')[0] || 'en';
+    this.tabController[this.defaultLanguage] = '';
+  }
 
   ngOnInit() {
-    console.log('eh chaval', this.userLanguage);
+    const parentTranslations = Object.assign({}, this.form.value.description);
+    Object.keys(parentTranslations).forEach((key) => (parentTranslations[key] === '') && delete parentTranslations[key]);
+    this.contentServiceInstance.getAvailableLanguages()
+      .subscribe((resp) => {
+        this.availableLanguages = resp.filter((lng) => lng !== this.defaultLanguage).slice();
+        Object.assign(this.tabController, parentTranslations);
+        this.availableLanguages = this.availableLanguages.filter(lng => !Object.keys(this.tabController).includes(lng));
+      });
+  }
+
+  getLanguages() {
+    return Object.keys(this.tabController);
+  }
+
+  getTranslation(target_lng) {
+    const text = this.form.value.description[this.defaultLanguage];
+    const translations = Object.assign({}, this.form.value.description);
+    this.store.dispatch(new ShowLoader());
+    this.contentServiceInstance.getTranslation(this.defaultLanguage, target_lng, text)
+      .subscribe((response) => {
+          translations[target_lng] = response.translation;
+          this.form.controls['description'].setValue(translations);
+          Object.keys(translations).forEach((key) => (translations[key] === '') && delete translations[key]);
+          this.tabController = Object.assign({}, translations);
+          this.availableLanguages = this.availableLanguages.filter(lng => !Object.keys(this.tabController).includes(lng));
+          this.store.dispatch(new HideLoader());
+        },
+        (error) => {
+          console.error(error);
+          this.store.dispatch(new HideLoader());
+        });
   }
 
 }
