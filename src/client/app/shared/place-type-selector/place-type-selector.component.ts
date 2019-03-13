d61130791735cc51e4f79206a0ba7883a51964eb
Enrico Martin <enrico@peakonestudios.com>
2019-03-13 16:04:28 -0300

    restyling icons

--- src/client/app/shared/place-type-selector/place-type-selector.component.ts
+++ src/client/app/shared/place-type-selector/place-type-selector.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TRANSLATE } from '../../translate-marker';
 import { PlaceType } from '../models/enum/PlaceType';
+import { eventColors } from '../models/TripTemplate';
 
 @Component({
   selector: 'app-place-type-selector',
@@ -9,12 +10,14 @@ import { PlaceType } from '../models/enum/PlaceType';
 })
 export class PlaceTypeSelectorComponent implements OnInit {
 
+  eventColors = eventColors;
+
   pointTypes = [
-    {value: PlaceType.POI, viewValue: TRANSLATE('POI'), enabled: false, icon: '/assets/icons/point_of_interest.png'},
-    {value: PlaceType.HOTEL, viewValue: TRANSLATE('HOTEL'), enabled: false, icon: '/assets/icons/hotel.png'},
-    {value: PlaceType.ACTIVITY, viewValue: TRANSLATE('ACTIVITY'), enabled: false, icon: '/assets/icons/activity.png'},
-    {value: PlaceType.TERMINAL, viewValue: TRANSLATE('TERMINAL'), enabled: false, icon: '/assets/icons/terminal.png'},
-    {value: PlaceType.DESTINATION, viewValue: TRANSLATE('DESTINATION'), enabled: false, icon: '/assets/icons/destination.png'},
+    {value: PlaceType.POI, viewValue: TRANSLATE('POI'), enabled: false, icon: 'photo_camera'},
+    {value: PlaceType.HOTEL, viewValue: TRANSLATE('HOTEL'), enabled: false, icon: 'hotel'},
+    {value: PlaceType.ACTIVITY, viewValue: TRANSLATE('ACTIVITY'), enabled: false, icon: 'local_activity'},
+    // {value: PlaceType.TERMINAL, viewValue: TRANSLATE('TERMINAL'), enabled: false, icon: ''},
+    // {value: PlaceType.DESTINATION, viewValue: TRANSLATE('DESTINATION'), enabled: false, icon: ''},
   ];
   @Input() enabledIcons = false;
   @Output() selectedTypes: EventEmitter<Array<any>> = new EventEmitter<Array<any>>();
